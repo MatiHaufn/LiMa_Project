c562c791ffc00ffd11c4168061cca3e556b9d62b
matheus <m.zacharska@web.de>
2023-04-23 12:06:32 +0200

    Beginn vom 3D 2D Switch Test mit Decals

--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -1,10 +1,13 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using System.Transactions;
-using UnityEngine;
-
+﻿using UnityEngine;
+using UnityEngine.Rendering.HighDefinition;
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField] GameObject _PlayerBody;
+    [SerializeField] GameObject _DecalProjectorObject;
+    [SerializeField] GameObject _WallCollider;
+    [SerializeField] DecalProjector _DecalProjector; 
+    bool _PlayerIs3D = true; 
+
     //Player Movement
     Rigidbody _myRigidbody;
 
@@ -13,22 +16,30 @@ public class PlayerMovement : MonoBehaviour
     public float _acceleration;
     float _startSpeed;
     float _halfSpeed;
-    float verticalVelocity;
+    float verticalVelocity = -1;
 
     private void Start()
     {
         _myRigidbody = this.gameObject.GetComponent<Rigidbody>();
+        
+        _PlayerBody.SetActive(_PlayerIs3D);
+        _DecalProjectorObject.SetActive(!_PlayerIs3D);
+
         _startSpeed = _speed;
         _halfSpeed = _speed * .75f;
     }
     private void FixedUpdate()
     {
-        Move(); 
+        Move();
+        SwitchDimension(); 
     }
     private void Move()
     {
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
+        
+        if (!_PlayerIs3D)
+            z = 0;
 
         if (x != 0 && z != 0)
             _speed = _halfSpeed;
@@ -38,5 +49,19 @@ public class PlayerMovement : MonoBehaviour
         move = transform.right * x * _acceleration + transform.forward * z * _acceleration;
 
         _myRigidbody.velocity = new Vector3(move.x * _speed, verticalVelocity, move.z * _speed);
+    }
+
+    private void SwitchDimension()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if(_WallCollider.GetComponent<WallTracker>().trackedWall == true && _PlayerIs3D == true)
+                _PlayerIs3D = false; 
+            else
+                _PlayerIs3D = true;
+
+            _PlayerBody.SetActive(_PlayerIs3D);
+            _DecalProjectorObject.SetActive(!_PlayerIs3D);
+        }
     }
 }
